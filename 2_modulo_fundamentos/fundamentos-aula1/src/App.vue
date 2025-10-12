225af992d45ed99e057d07abd5f5e89adc4fb31a
JeffSC <jeff.silva.caires@gmail.com>
2025-10-12 16:18:11 -0300

    modulo 2: aula 1

---
+++ 2_modulo_fundamentos/fundamentos-aula1/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+const title = "Modulo 2";
+const nomeDoAluno = '<span style="color: blue;">Jefferson</span>';
+const btnDisabled = false;
+</script>
+
+<template>
+    <div>
+        <h1 :title="title">Curso Vue.js Expert - {{ title }}</h1>
+        <p v-html="nomeDoAluno"></p>
+        <button :disabled="btnDisabled">Click me</button>
+    </div>
+</template>
